619e6d3af8f569af716c3817fe82131f6fe56f37
yasminama23-meet <109900009+yasminama23-meet@users.noreply.github.com>
2022-08-05 09:36:43 +0300

    .

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-rom flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
@@ -12,8 +12,8 @@ Config = {
   "storageBucket": "wishingwell-7d53a.appspot.com",
   "messagingSenderId": "308934572829",
   "appId": "1:308934572829:web:7ab3dc2485b45a07213ff4",
-  "measurementId": "G-QXCYKZ7R2S"
-  "databaseURL":""
+  "measurementId": "G-QXCYKZ7R2S",
+  "databaseURL": "https://wishingwell-7d53a-default-rtdb.firebaseio.com/"
 }
 
 
@@ -33,25 +33,71 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
 
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+ 
 
 
-@app.route('/signup')
+
+
+
+@app.route('/signup', methods=['GET','POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        username = request.form['username']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+
+            user = {"usename": username, "email": email, "password": password}
+            db.child("Users").child(login_session['user']['localId']).set(user)
+
+            return redirect(url_for('index'))
+        except:
+            error = "Authentication failed"
+            return error
     return render_template('signup.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('index'))
+        except:
+            error = "Authentication failed"
+            return error
+    else: 
+        return render_template('login.html')
 
-@app.route('/login')
-def loin():
-    return render_template('login.html')
+
+
+
+
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
+@app.route('/cherish')
+def cherish():
+    return render_template('cherish.html')
+
+
+@app.route('/card')
+def card():
+    return render_template('card.html')
 
 #Code goes above here
 
